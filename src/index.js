1ff4ca4ae7d5c3d824c17a0d3bcfe5f49cdb4919
Rifat Mullayanov <rmullayanov@vodohod.ru>
2021-10-11 17:24:13 +0300

    master

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ const createWindow = () => {
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
 
   mainWindow.setMenuBarVisibility(false)
 
